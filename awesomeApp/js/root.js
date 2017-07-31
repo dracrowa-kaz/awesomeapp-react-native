9382a7b6c4a228422b83082aa743e98473bd12e8
sato kazuki <dracrowa@gmail.com>
2017-07-31 22:15:58 +0300

    googlemap, test

--- awesomeApp/js/root.js
+++ awesomeApp/js/root.js
@@ -11,14 +11,20 @@ import { connect } from 'react-redux';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import { addTodo } from './Todo'
 
-//export function addTodo(name, dueTo) {
+
 class AwesomeApp extends Component {
   static navigationOptions = {
     title: 'AwesomeApp',
   };
   constructor(props) {
     super(props)
+    this.state = {
+      markers: [ {latlng:{latitude: 60.4518,longitude: 22.2666}, title:"test", description:"desc"} ]
+    }
     this.pushed = this.pushed.bind(this)
+    this.state.markers.map(marker => (
+      console.log(marker.latlng)
+    ))
   }
   pushed(){
     this.props.dispatch(addTodo("test","aaa"))
@@ -26,28 +32,41 @@ class AwesomeApp extends Component {
     navigate('ChatList', { name: 'Jane' })
   }
   render() {
-    return (
+      const ASPECT_RATIO = width / height
+      const LATITUDE = 38.9637
+      const LONGITUDE = 35.2433
+      const LATITUDE_DELTA = 85
+      const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+      return (
         <View style={styles.container}>
-          <TouchableOpacity style={{ height: viewHeight, width:40, justifyContent: 'center', alignItems: 'center' }} onPress={this.pushed}>
-            <Text>Push me</Text>
-          </TouchableOpacity>
           <MapView
             provider={MapView.PROVIDER_GOOGLE}
             style={styles.map}
             initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
+              latitude: LATITUDE,
+              longitude: LONGITUDE,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
             }}
-          />
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+            full
+          >
+          {this.state.markers.map(marker => (
+            <MapView.Marker
+              key={marker.latlng.longitude}
+              coordinate={marker.latlng}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+          </MapView>
         </View>
     );
   }
 }
 
 function mapStateToProps(state) {
-  console.log(state)
   const {todo} = state
   return {
     todo,
@@ -55,20 +74,14 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(AwesomeApp)
-/*
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(awesomeApp);
-*/
 
 var {height, width} = Dimensions.get('window');
-var viewHeight = 100
+var viewHeight = 0
 const styles = StyleSheet.create({
   map: {
+    flex:1,
     height: height - viewHeight,
     width: width
-    //...StyleSheet.absoluteFillObject,
   },
   container: {
     flex: 1,
